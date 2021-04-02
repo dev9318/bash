15faf41bae3dd57b0f519bf586a00fa536a547c2
Dev <dev.moxaj@gamil.com>
2021-04-02 17:07:14 +0530

    added operations

--- operation.sh
+++ operation.sh
@@ -16,8 +16,8 @@
 #    echo "bye bye"
 #fi
 
-num1=20
-num2=5
+#num1=20
+#num2=5
 
 #echo $(expr $num1 + $num2 )
 #echo $(expr $num1 - $num2 )
@@ -28,4 +28,16 @@ num2=5
 #echo $(( $num1 + $num2 ))
 #echo $(( $num1 - $num2 ))
 
-echo $(expr $num1 \* $num2 )
+#echo $(expr $num1 \* $num2 )
+
+num1=20.5
+num2=5
+#echo "20.5+5" | bc
+#echo "scale=2;20.5/5" | bc #scale implies that many decimal places
+
+# echo "$num1-$num2" | bc
+
+echo "scale=3;sqrt($num1)" | bc -l
+echo "3^3" | bc -l
+
+
